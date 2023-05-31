3ad2c4ffb37b30d00196680a41233b04faea92d6
ALEX\admin <letsdevjava@gmail.com>
2023-05-30 20:17:13 -0600

    add variabless class

---
+++ src/main/java/org/comit/course/Main.java
@@ -0,0 +1,7 @@
+package org.comit.course;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/comit/course/_01_variables/Variables.java
@@ -0,0 +1,10 @@
+package org.comit.course._01_variables;
+
+public class Variables {
+    public static int num3;
+    public static void main(String[] args) {
+        int num = '5';
+        int num2 = num3 + num - 2;
+        System.out.println("The value is " + num2);
+    }
+}
